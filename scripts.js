5b7420aa671ec0a11ca6068b0a87c554f6517fee
pcerimeli <mecacerimeli@gmail.com>
2020-02-24 11:25:48 -0300

    Maratona Dev

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document
+                .querySelector('header button')
+                .addEventListener("click",function 
+                (params) {
+                  document
+                                .querySelector('.form')
+                                .classList.toggle('hide')
+                  
+                })
+
